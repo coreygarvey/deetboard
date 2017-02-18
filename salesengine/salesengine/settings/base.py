abc8155173d81b85c619a1a73efadbb07b4f6ff9
Corey Garvey <CGarv16@gmail.com>
2017-02-18 19:19:00 +0000

    Landing page built

--- salesengine/salesengine/settings/base.py
+++ salesengine/salesengine/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'widget_tweaks',
     'rest_framework',
     'accounts',
     'orgs',
