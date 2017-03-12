bfbef885083e067823447160e563b736e9b6826e
Corey Garvey <CGarv16@gmail.com>
2017-03-12 17:17:13 +0000

    added feature checkboxes to question page

--- salesengine/questions/forms.py
+++ salesengine/questions/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from models import Question
+from products.models import Feature
 from django.utils.translation import ugettext_lazy as _
 import re
 
 class QuestionForm(forms.ModelForm):
-	text = forms.CharField(required=False, widget=forms.TextInput(attrs=dict(required=False, max_length=30)), label=_("Text"))
+	text = forms.CharField(required=True, widget=forms.TextInput(attrs=dict(required=False, max_length=30)), label=_("Text"))
 	class Meta:
 		model = Question
 		fields = ['text']
@@ -19,3 +20,22 @@ class QuestionForm(forms.ModelForm):
 		cleaned_data = super(QuestionForm,self).clean()
 
 		super(QuestionForm, self).clean()
+
+
+class QuestionFeaturesForm(forms.ModelForm):
+	features = forms.ModelMultipleChoiceField(required=True, widget=forms.CheckboxSelectMultiple(), queryset=Feature.objects.all())
+
+	class Meta:
+		model = Question
+		fields = ['features']
+	
+	def __init__(self, *args, **kwargs):
+		self.request = kwargs.pop('request', None)
+		super(QuestionFeaturesForm, self).__init__(*args, **kwargs)
+		print kwargs
+
+
+	def clean(self):
+		cleaned_data = super(QuestionFeaturesForm,self).clean()
+
+		super(QuestionFeaturesForm, self).clean()
--- salesengine/questions/views.py
+++ salesengine/questions/views.py
@@ -7,7 +7,7 @@ from django.views.generic import CreateView, TemplateView
 from django.http import HttpResponse
 from django.views import View
 import services
-from forms import QuestionForm
+from forms import QuestionForm, QuestionFeaturesForm
 from braces.views import LoginRequiredMixin
 from orgs.models import Org
 from products.models import Product, Feature
@@ -75,10 +75,11 @@ class QuestionCreateView(LoginRequiredMixin, CreateView):
         				)
         			)
 
-class QuestionView(TemplateView):
+class QuestionView(CreateView):
     """
     
     """
+    form_class = QuestionFeaturesForm
     template_name = "questions/question-home.html"
 
     def get_context_data(self, **kwargs):
@@ -105,6 +106,34 @@ class QuestionView(TemplateView):
         context['question_features'] = question_features
         print context
         return context
+
+    def form_valid(self, form):        
+        question = form.save(commit=False)
+        question.save()
+
+        return super(QuestionView, self).form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super(QuestionView, self).get_form_kwargs()
+        kwargs.update({
+            'request' : self.request
+        })
+        return kwargs
+
+    def get_success_url(self):
+        question_pk = self.object.id
+        question = Question.objects.get(pk=question_pk)
+        if 'fpk' in self.kwargs:
+            feature_pk = self.kwargs['fpk']
+            feature = Feature.objects.get(pk=feature_pk)
+            question.features.add(feature)
+        return reverse('question_home', 
+                    args=(
+                        self.object.product.org.id,
+                        self.object.product.id,
+                        self.object.id
+                        )
+                    )
 
 class QuestionListView(TemplateView):
     """
